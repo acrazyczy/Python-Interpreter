b0747ce059b6858d811abfdb3d6df166d5e8f6ce
acrazyczy <a_crazy_czy@yahoo.com>
2019-12-11 09:00:48 +0800

    Fix namespace.(Ver 3.0)

--- src/Name_Space/Name_Space.cpp
+++ src/Name_Space/Name_Space.cpp
@@ -15,11 +15,11 @@ dtype &name_space::operator[](const std::string &var_name)
 {
 	std::map<std::string , dtype>::iterator it = static_var_sys.find(var_name);
 	if (it == static_var_sys.end())
-		if (is_global_block) it = static_var_sys.insert(std::make_pair(var_name , dtype())).first , crt.top().push_back(var_name);;
+		if (is_global_block) it = static_var_sys.insert(std::make_pair(var_name , dtype())).first , crt.top().push_back(var_name);
 		else
 		{
 			it = var_sys.find(var_name);
-			if (it == var_sys.end()) it = var_sys.insert(std::make_pair(var_name , dtype())).first , crt.top().push_back(var_name);;
+			if (it == var_sys.end()) it = var_sys.insert(std::make_pair(var_name , dtype())).first , crt.top().push_back(var_name);
 		}
 	return it -> second;
 }
